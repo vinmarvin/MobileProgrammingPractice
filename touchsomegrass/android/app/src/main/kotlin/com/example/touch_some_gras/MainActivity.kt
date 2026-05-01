3a2b9214a52ef1410c1b0c98b8ef0bf55d8a9aa9
Ryan M S <146501417+vinmarvin@users.noreply.github.com>
2026-05-01 08:51:38 +0700

    Delete touchsomegrass directory

--- touchsomegrass/android/app/src/main/kotlin/com/example/touch_some_gras/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.touch_some_gras
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- touchsomegrass/android/app/src/main/kotlin/com/example/touch_some_grass/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.touch_some_grass
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
